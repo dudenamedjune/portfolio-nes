228e1cae2e927c60eea74762c10e8e5dd475f797
dudenamedjune <dudenamedjune@gmail.com>
2020-08-03 00:05:58 -0500

    added server

---
+++ server.js
@@ -0,0 +1,25 @@
+
+var  express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("./build"));
+
+
+
+app.get("*", function(req, res) {
+  res.sendFile(__dirname + "/build/index.html");
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
